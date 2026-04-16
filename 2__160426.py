786fb7c5fc0a7e8cb2715bcf55b88fbfd073be06
VimeRJ <i.vimerj@gmail.com>
2026-04-16 22:07:58 +0300

    commit

--- 2__160426.py
+++ 2__160426.py
@@ -1,2 +1,13 @@
-num_medium = list(int(input("Enter a number: >>>")))
-print(num_medium)
+numbers = []
+
+while True:
+    user_input = input("Enter a number (or 'done' to finish): ")
+    if user_input.lower() == 'done':
+        break
+    try:
+        numbers.append(int(user_input))
+    except ValueError:
+        print("Please enter a valid number.")
+
+print(f"Amount of numbers: {len(numbers)}")
+print(f"Numbers: {numbers}")
