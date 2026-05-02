2e91139aa214b75fce2dc621e7f5668170360acc
VimeRJ <i.vimerj@gmail.com>
2026-05-02 22:26:03 +0300

    commit1

--- goit-pycore-hw-04/1.py
+++ goit-pycore-hw-04/1.py
@@ -1,4 +1,28 @@
+import re
 
-with open("salary.txt", "r") as file:
-        lines = file.readlines()
-print(lines)
+
+def total_salary(path):
+    total = 0
+    list_avg = []
+    path = "salary.txt"
+    
+    with open(path, "r") as file:
+        while True:
+            line = file.readline()
+            salary_avg = re.search(r'\d+', line)
+
+            if salary_avg:
+                list_avg.append(int(salary_avg.group()))
+            if not line:                
+                break
+            salary = re.search(r'\d+', line)
+            if salary:
+                total += int(salary.group())
+    
+    avg = int(total / len(list_avg))
+    salaries_list = [total, avg]
+    
+    return salaries_list
+
+print(total_salary('salary.txt'))
+print(f"Total salary: {total_salary('salary.txt')[0]}, Average salary: {total_salary('salary.txt')[1]}")
